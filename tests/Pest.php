88282ff2b48fc2c0ae8e4b6aafaa10e98bd4f428
Lloric Mayuga Garcia <lloricode@gmail.com>
2023-02-16 08:40:55 +0800

    wip
    Signed-off-by: Lloric Mayuga Garcia <lloricode@gmail.com>

--- tests/Pest.php
+++ tests/Pest.php
@@ -2,15 +2,19 @@
 
 declare(strict_types=1);
 
+use Illuminate\Support\Facades\Http;
 use Lloricode\LaravelPaymaya\Tests\TestCase;
 
 uses(TestCase::class)
-    ->beforeEach(fn () => config(
-        [
+    ->beforeEach(function () {
+        if (version_compare(app()->version(), '9.0.0', '>=')) {
+            Http::preventStrayRequests();
+        }
+        config([
             'paymaya-sdk.keys' => [
                 'public' => 'public-xxx',
                 'secret' => 'secret-xxx',
             ],
-        ]
-    ))
+        ]);
+    })
     ->in(__DIR__);
