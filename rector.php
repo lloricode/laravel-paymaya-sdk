ba94b1d9ef2c621c05044f1cd4652c85a6855f97
Lloric Mayuga Garcia <lloricode@gmail.com>
2025-08-24 12:51:45 +0800

    Update PHP version to 8.3, optimize workflows, and enhance code style rules

--- rector.php
+++ rector.php
@@ -3,7 +3,6 @@
 declare(strict_types=1);
 
 return Rector\Config\RectorConfig::configure()
-    ->withParallel(maxNumberOfProcess: 6)
     ->withPhpSets()
     ->withPaths([
         __DIR__.'/src',
